67d96b14f754a409299992e6bda490794c7904ce
Jean Grijp <grijpjean@gmail.com>
2021-02-23 13:51:57 -0300

    .

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export default function Profile () {
+  return (
+    <div>
+        <img src="https://github.com/account" alt="Jean Grijp"/>
+        <div>
+          <strong>Jean Grijp</strong>
+          <p>Level 1</p>
+        </div>
+    </div>
+  )
+}
