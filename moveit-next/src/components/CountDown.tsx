8888fc506c5356f378b77d7ea3ee665276b85b87
Jean Grijp <grijpjean@gmail.com>
2021-02-24 17:27:02 -0300

    .

--- moveit-next/src/components/CountDown.tsx
+++ moveit-next/src/components/CountDown.tsx
@@ -3,8 +3,9 @@ import styles from '../styles/components/CountDown.module.css';
 
 export function CountDown () {
   const [time, setTime] = useState(25*60);
-  const [active, setActive] = useState(false)
+  const [isActive, setIsActive] = useState(false)
   
+  let countDownTimeout: NodeJS.Timeout
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -12,20 +13,23 @@ export function CountDown () {
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-  function startCountDown () {
-    setActive(true)
-  }
   useEffect(() => {
-      if (active && time > 0) {
-        setTimeout(() => {
+    if (isActive && time > 0) {
+      countDownTimeout = setTimeout(() => {
           setTime(time - 1)
         }, 1000)
       }
-  }, [active, time])
+    }, [isActive, time])
 
+  function startCountDown () {
+    setIsActive(true)
+  }
 
-
-
+  function resetCountDown () {
+    clearTimeout(countDownTimeout)
+    setIsActive(false);
+    setTime(25 * 60)
+  }
 
   return (
     <>
@@ -40,10 +44,22 @@ export function CountDown () {
         <span>{secondRight}</span>
       </div>
     </div>
-    <button 
-    className={styles.countDownButton}
-    onClick={startCountDown}
-    >Iniciar um ciclo</button>
+
+    { isActive ? (
+      <button 
+      className={`${styles.countDownButton} ${styles.countDownButtonActive}`}
+      onClick={resetCountDown}
+      >Abandonar ciclo
+      </button>
+
+    ) : (
+      <button 
+      className={styles.countDownButton}
+      onClick={startCountDown}
+      >Iniciar um ciclo
+      </button>
+    )
+  }
     </>
   )
 }
