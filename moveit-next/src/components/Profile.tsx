fa9f6faedef090a493bbf494a1c00b547e9a9be5
Jean Grijp <grijpjean@gmail.com>
2021-02-24 00:13:23 -0300

    .

--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,10 +1,15 @@
+import styles from '../styles/components/Profile.module.css';
+
 export default function Profile () {
   return (
-    <div>
-        <img src="https://github.com/account" alt="Jean Grijp"/>
+    <div className={styles.profileContainer}>
+        <img src="https://avatars.githubusercontent.com/u/40359722?s=460&u=03571e489cb46d8893e98692f6d2d531cc974d4f&v=4" alt="Jean Grijp"/>
         <div>
           <strong>Jean Grijp</strong>
-          <p>Level 1</p>
+          <p>
+            <img src="icons/level.svg" alt=""/>
+            Level 1
+          </p>
         </div>
     </div>
   )
--- moveit-next/src/pages/index.tsx
+++ moveit-next/src/pages/index.tsx
@@ -1,14 +1,22 @@
 import { ExperienceBar } from "../components/ExperienceBar";
 import Head from 'next/head'
 import Profile from "../components/Profile";
+import styles from '../styles/pages/Home.module.css';
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { CountDown } from "../components/CountDown";
 
 export default function Home() {
   return (
-    <div className='container'>
+    <div className={styles.container}>
+      <Head>
+        <title>Início | Move.it</title>
+      </Head>
     <ExperienceBar />
     <section>
       <div>
         <Profile/>
+        <CompletedChallenges/>
+        <CountDown/>
       </div>
       <div>
 
